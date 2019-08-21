8e1038d5619fd5c606089abe915b66f1f8f78a3c
Elliot Penson <elliot.penson@it.com>
2019-08-20 21:29:25 -0400

    Bootstrap project with create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Nothing to see here!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
