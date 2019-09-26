867cd1273eb4f061a3c769789abbc4e805f3962c
Elliot Penson <elliot.penson@it.com>
2019-09-26 13:53:20 -0400

    Display a colored header on the view page

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -1,13 +1,20 @@
+import { Box, Heading } from "grommet";
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 
-import { languages } from "../../languages";
+import { languages, findColor } from "../../languages";
+import { findTextColor } from "../../utils";
 
 export function View({ location }: RouteComponentProps) {
   const key = location.pathname.replace("/", "");
   const language = languages.find(language => language.key === key);
   if (language) {
-    return <div className="Language">{language.name}</div>;
+    const color = findColor(language);
+    return (
+      <Box pad="large" background={{ color }}>
+        <Heading color={findTextColor(color)}>{language.name}</Heading>
+      </Box>
+    );
   } else {
     return <div>Not found!</div>;
   }
